848aacbb97b92a9024d74fe968279bf783eae2a4
AlGitHub23 <122891951+AlGitHub23@users.noreply.github.com>
2023-01-19 21:27:47 +0500

    Add files via upload

---
+++ Printer.java
@@ -0,0 +1,36 @@
+public class Printer {
+    private String queue = "";
+    private int pagesInQueue;
+    private int printedPages;
+    public void append(String text, String name, int pagesCount) {
+        queue = queue + "\n" + name + " - " + text;
+        pagesInQueue = pagesInQueue + pagesCount;
+    }
+    public void append(String text, String name) {
+        queue = queue + "\n" + name + " - " + text;
+        pagesInQueue = pagesInQueue + 1;
+    }
+    public void append(String text, int pagesCount) {
+        queue = queue + "\n" + "Документ без имени" + " - " + text;
+        pagesInQueue = pagesInQueue + pagesCount;
+    }
+    public void append(String text) {
+        queue = queue + "\n" + "Документ без имени" + " - " + text;
+        pagesInQueue = pagesInQueue + 1;
+    }
+    public void clear() {
+        pagesInQueue = 0;
+        queue = "";
+    }
+    public void print() {
+       printedPages = printedPages + pagesInQueue;
+        System.out.println(queue);
+        clear();
+    }
+    public int getPendingPagesCount () {
+        return pagesInQueue;
+    }
+    public int getPrintedPagesCount () {
+        return printedPages;
+    }
+}
